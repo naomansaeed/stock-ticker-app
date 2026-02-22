7659bf97921fb86350c047657c979df008d2bd85
Naoman Saeed <naoman.saeed@gmail.com>
2026-02-22 13:02:01 +0500

    feat: implemented the script to dynamically update values.

--- fakeStockAPI.js
+++ fakeStockAPI.js
@@ -1,9 +1,9 @@
-const now = new Date();
 export function getStockData () {
+    const now = new Date();
     return {
         name: 'QtechAI',
         sym: 'QTA',
-        price: Math.random() * 3 /* return a random number between 0 and 3 to two decimal places */,
+        price: Math.random() * 4 /* return a random number between 0 and 3 to two decimal places */,
         time: now.toLocaleTimeString('en-PK',{hour12:false}) /* return a timestamp in this format: hh/mm/ss */
     }
 }
--- script.js
+++ script.js
@@ -3,20 +3,46 @@ import { getStockData } from './fakeStockAPI.js';
 // Select elements
 const nameEl = document.getElementById("name-el");
 const symbolEl = document.getElementById("symbol-el");
-const priceEl = document.getElementById("price-el");
+//const priceEl = document.getElementById("price-el");
 const timeEl = document.getElementById("time-el");
 const trendEl = document.getElementById("trend-icon");
+const price = document.getElementById("price-value");
 
 
 
-let previousPrice = 0;
+let previousPrice = null;
 // Call function
 
 function updateStock() {
+    const data = getStockData();
     // Update DOM
-    nameEl.textContent = getStockData().name;
-    symbolEl.textContent = getStockData().sym;
-    priceEl.textContent = getStockData().price;
-    timeEl.textContent = getStockData().time;
+    nameEl.textContent = data.name;
+    symbolEl.textContent = data.sym;
+    //priceEl.querySelector('#price-value').textContent = data.price.toFixed(2);
+    price.textContent = data.price.toFixed(2);
+    timeEl.textContent = data.time;
+
+    //compare previous price
+    if(previousPrice!==null){
+
+        trendEl.classList.remove('trend-up', 'trend-down', 'trend-unchanged');
+
+        if(data.price > previousPrice){
+            trendEl.textContent = "  ▲";
+            trendEl.classList.add('trend-up');
+        }
+        else if (data.price < previousPrice) {
+            trendEl.textContent = "  ▼";
+            trendEl.classList.add('trend-down');
+        }
+        else{
+            trendEl.textContent = "  ►";
+            trendEl.classList.add('trend-unchanged');
+        }
+    }
+
+    previousPrice = data.price;
 }
 
+updateStock();
+setInterval(updateStock,1500);
